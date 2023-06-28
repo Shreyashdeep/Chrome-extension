dfa591752fd8e277a9dcbe00b272572cb9062f99
Shreyashdeep <shreyashdeep20@gmail.com>
2023-06-28 05:30:26 +0530

    saving the prompt

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ let myLeads = []
 const inputEl= document.getElementById("input-el")
 const inputBTn= document.getElementById("input-btn")
 const ulEl= document.getElementById("ul-el")
+let leadsFromLocalStorage= JSON.parse( localStorage.setItem("myLeads"))
+
+localStorage.setItem("myLeads", "www.examplelead.com")
 
 inputBTn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value=""
+    localStorage.setItem("myLeads" , JSON.stringify(myLeads))
     renderLeads()
     
 })
